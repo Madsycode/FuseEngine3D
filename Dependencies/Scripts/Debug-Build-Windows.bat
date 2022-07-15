c634dacfb4535c50fd98c8953e57235c207c6d7e
Franc Pouhela <pouhela@kl.dfki.de>
2022-07-15 15:19:45 +0200

    cmake post-build command

--- Dependencies/Scripts/Debug-Build-Windows.bat
+++ Dependencies/Scripts/Debug-Build-Windows.bat
@@ -4,10 +4,4 @@ cmake -O . -B ./Build -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=ON
 rem build project
 cmake --build ./Build
 
-rem copy shared .dll files to ouput directory
-robocopy "Dependencies/Libraries/Shared/bin/"  "Build/Debug/" /E
-
-rem copy debug .dll files to ouput directory
-robocopy "Dependencies/Libraries/Debug/bin/"  "Build/Debug/" /E
-
 echo compilation done!
